2b0cb91e7fb4e85fa7105fd3e9eabb3960a89a60
Coletinn <gustavocoleto@hotmail.com>
2021-06-29 14:57:31 -0300

    Criando operacao de instanciacao

--- src/main/java/com/coletocorporation/coursemc/CoursemcApplication.java
+++ src/main/java/com/coletocorporation/coursemc/CoursemcApplication.java
@@ -1,13 +1,32 @@
 package com.coletocorporation.coursemc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CoursemcApplication {
+import com.coletocorporation.coursemc.domain.Categoria;
+import com.coletocorporation.coursemc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CoursemcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
