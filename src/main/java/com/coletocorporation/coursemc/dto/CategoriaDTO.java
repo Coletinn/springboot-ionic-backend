0f710572579679d949b6323f9082a6ea827da101
Coletinn <gustavocoleto@hotmail.com>
2021-07-05 00:22:49 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/coletocorporation/coursemc/dto/CategoriaDTO.java
+++ src/main/java/com/coletocorporation/coursemc/dto/CategoriaDTO.java
@@ -2,6 +2,9 @@ package com.coletocorporation.coursemc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.coletocorporation.coursemc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -9,11 +12,15 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
-
+	
 	public CategoriaDTO() {
 		
 	}
+ 	
 	
 	public CategoriaDTO(Categoria obj) {
 		id = obj.getId();
--- src/main/java/com/coletocorporation/coursemc/services/CategoriaService.java
+++ src/main/java/com/coletocorporation/coursemc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.coletocorporation.coursemc.domain.Categoria;
+import com.coletocorporation.coursemc.dto.CategoriaDTO;
 import com.coletocorporation.coursemc.repositories.CategoriaRepository;
 import com.coletocorporation.coursemc.services.exceptions.DataIntegrityException;
 import com.coletocorporation.coursemc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
