5d97b6aa44ef5089a89900fbf04d71f8eadeb6a2
Coletinn <gustavocoleto@hotmail.com>
2021-07-04 21:58:10 -0300

    Deletando uma Categoria com DELETE

---
+++ src/main/java/com/coletocorporation/coursemc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,10 @@
+package com.coletocorporation.coursemc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+}
