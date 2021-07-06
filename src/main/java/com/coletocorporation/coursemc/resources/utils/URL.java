8ccbb8cadcafdbdcf9e96ead70544c229e5e7c4c
Coletinn <gustavocoleto@hotmail.com>
2021-07-06 10:53:57 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/coletocorporation/coursemc/resources/utils/URL.java
@@ -0,0 +1,32 @@
+package com.coletocorporation.coursemc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+		
+		public static String decodeParam(String str) {
+			
+			try {
+				return URLDecoder.decode(str, "UTF-8");
+			}
+			catch (UnsupportedEncodingException e) {
+				return "";
+			}
+			
+		}
+		
+		public static List<Integer> decodeIntList(String str) {
+		
+		String[] vet = str.split(",");
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < vet.length; i++) {
+			Integer numbers = Integer.parseInt(vet[i]);
+			list.add(numbers);
+		}
+		return list;
+	}
+
+}
