4e7d313f0f7ec17dd8ab0a5fa4dc4fa0e7989c1e
Coletinn <gustavocoleto@hotmail.com>
2021-07-13 14:25:51 -0300

    Restricao de conteudo: Cliente so recupera seus pedidos

--- src/main/java/com/coletocorporation/coursemc/repositories/PedidoRepository.java
+++ src/main/java/com/coletocorporation/coursemc/repositories/PedidoRepository.java
@@ -1,11 +1,16 @@
 package com.coletocorporation.coursemc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.coletocorporation.coursemc.domain.Cliente;
 import com.coletocorporation.coursemc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/coletocorporation/coursemc/services/PedidoService.java
+++ src/main/java/com/coletocorporation/coursemc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.coletocorporation.coursemc.domain.Cliente;
 import com.coletocorporation.coursemc.domain.ItemPedido;
 import com.coletocorporation.coursemc.domain.PagamentoComBoleto;
 import com.coletocorporation.coursemc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.coletocorporation.coursemc.domain.enums.EstadoPagamento;
 import com.coletocorporation.coursemc.repositories.ItemPedidoRepository;
 import com.coletocorporation.coursemc.repositories.PagamentoRepository;
 import com.coletocorporation.coursemc.repositories.PedidoRepository;
+import com.coletocorporation.coursemc.security.UserSS;
+import com.coletocorporation.coursemc.services.exceptions.AuthorizationException;
 import com.coletocorporation.coursemc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -66,5 +72,17 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
