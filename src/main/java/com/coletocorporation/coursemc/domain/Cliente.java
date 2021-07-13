1956ab4f66631aa6ff97308d3c70eef9211d8f3a
Coletinn <gustavocoleto@hotmail.com>
2021-07-13 08:59:57 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/coletocorporation/coursemc/domain/Cliente.java
+++ src/main/java/com/coletocorporation/coursemc/domain/Cliente.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.coletocorporation.coursemc.domain.enums.Perfil;
 import com.coletocorporation.coursemc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -42,15 +45,19 @@ public class Cliente implements Serializable {
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
-		
+		addPerfil(Perfil.CLIENTE);
 	}
 	
 	
@@ -63,6 +70,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo == null) ? null : tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 
@@ -139,7 +147,13 @@ public class Cliente implements Serializable {
 		this.senha = senha;
 	}
 	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
 	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
+	}
 
 	@Override
 	public int hashCode() {
--- src/main/java/com/coletocorporation/coursemc/services/DBService.java
+++ src/main/java/com/coletocorporation/coursemc/services/DBService.java
@@ -20,6 +20,7 @@ import com.coletocorporation.coursemc.domain.PagamentoComCartao;
 import com.coletocorporation.coursemc.domain.Pedido;
 import com.coletocorporation.coursemc.domain.Produto;
 import com.coletocorporation.coursemc.domain.enums.EstadoPagamento;
+import com.coletocorporation.coursemc.domain.enums.Perfil;
 import com.coletocorporation.coursemc.domain.enums.TipoCliente;
 import com.coletocorporation.coursemc.repositories.CategoriaRepository;
 import com.coletocorporation.coursemc.repositories.CidadeRepository;
@@ -123,17 +124,21 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "gucoletocursos@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Cliente cli2 = new Cliente(null, "Ana Costa", "coletogustavo@gmail.com", "9938383933", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93888321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
 		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
