a8e8a727f5d12dffbc5a7885846fe162fe5b18ca
Coletinn <gustavocoleto@hotmail.com>
2021-07-14 17:48:33 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/coletocorporation/coursemc/CoursemcApplication.java
+++ src/main/java/com/coletocorporation/coursemc/CoursemcApplication.java
@@ -1,12 +1,17 @@
 package com.coletocorporation.coursemc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.coletocorporation.coursemc.services.S3Service;
+
 @SpringBootApplication
 public class CoursemcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
@@ -14,7 +19,7 @@ public class CoursemcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\workspaces\\temp\\fotos\\notebook.jpg");
 	}
 
 }
