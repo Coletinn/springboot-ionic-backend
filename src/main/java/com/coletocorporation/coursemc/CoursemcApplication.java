cb69d1d18e35289b22ada009d76cf2a856c518fc
Coletinn <gustavocoleto@hotmail.com>
2021-07-14 19:24:24 -0300

    Enviando imagem via endpoint

--- src/main/java/com/coletocorporation/coursemc/CoursemcApplication.java
+++ src/main/java/com/coletocorporation/coursemc/CoursemcApplication.java
@@ -1,17 +1,11 @@
 package com.coletocorporation.coursemc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.coletocorporation.coursemc.services.S3Service;
-
 @SpringBootApplication
 public class CoursemcApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
@@ -19,7 +13,7 @@ public class CoursemcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\workspaces\\temp\\fotos\\notebook.jpg");
+		
 	}
 
 }
--- src/main/java/com/coletocorporation/coursemc/services/ClienteService.java
+++ src/main/java/com/coletocorporation/coursemc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.coletocorporation.coursemc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.coletocorporation.coursemc.domain.Cidade;
 import com.coletocorporation.coursemc.domain.Cliente;
@@ -36,6 +38,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 
 public Cliente find(Integer id) {
 		
@@ -106,5 +111,9 @@ public Cliente find(Integer id) {
 		
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
